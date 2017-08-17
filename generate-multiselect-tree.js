a3c0b410f2c20840ae4af76c484ecdb4ff96817e
Tom Cartwright <tom.cartwright@barrachd.com>
2017-08-17 17:04:41 +0100

    submenus now working with less of a hack

--- generate-multiselect-tree.js
+++ generate-multiselect-tree.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
     });
 
     var generateMenuFromJSON = function(json) {
-        var parent_menu = $('#mydropdownmenu'); //sets starting point for menu
+        var parent_menu = $('#mydropdownmenu');
         var menu_items = json;//$.parseJSON($(json)); //gets list of items in first level of menu
         generateMenu(menu_items, parent_menu); //draws the elements
     };
 
     var generateMenu = function(menu_items, parent_menu) {
-        //loop through json in a depth first way generating content
+        //loop through json in a depth first search generating the menus and submenus
         for(var i=0; i<menu_items.length; i++) {
             menu_item = menu_items[i];
             if (typeof menu_item.children.length === typeof undefined) { //deciding whether it is the end of a branch or not
@@ -22,53 +22,7 @@ $(document).ready(function() {
         }
     };
 
-    var generateSubMenuItem = function(menu_item, parent_menu) {
-
-        //construct children string
-        if (typeof menu_item.children.length !== typeof undefined) {
-            var children = [];
-            for(var i=0; i < menu_item.children.length; i++) {
-                children.push('{"name":"' + menu_item.children[i].name + '"}');
-            }
-            var children_string = '[ ' + children.join(',') + ']';
-
-        } else {
-          console.log("hit");
-        }
-        /*
-        Example Generate HTML
-        <li class="dropdown-submenu">
-            <li>
-                <a href="#" class="small" tabIndex="-1">
-                  <input type="checkbox" name="1" children='[{"name": "3.1"}, {"name": "3.2"}, {"name":"3.3"}]'>
-                  1
-                </a>
-            </li>
-            <ul class="dropdown-menu">
-                <!-- This is the submenu -->
-            </ul>
-        </li>
-        */
-        $(parent_menu)
-            .append($('<li>', {class : "dropdown-submenu"})
-                .append($('<a>', { class: "small", tabIndex : "-1", href : "#" })
-                    .append($('<input>', { class: "menu-checkbox", name : menu_item.name, type : "checkbox", "data-children" : children_string, onclick : "checkboxChanged(this)"}))
-                    .append($('<span>', {text: ' ' + menu_item.name})))
-                .append($('<ul>', {id: 'ul_' + menu_item.name.replace(/\s/g, ''), class : "dropdown-menu"})));
-
-        //set new parent to be the sub_menu (using the name attribute)
-        // TODO: the issues that were arising was with the selection of the parent element for Sector2 (it was selecting sector1)
-        // this was fixed but with a hack in the JSON - I need to replace the var submenu=$(submenu_id) line with something that will work indepenet of json
-        // e.g. something saving the <ul> element as it is made above and then putting that through as the sub menu
-        if (typeof menu_item.children.length !== typeof undefined) {
-            var submenu_id = '#ul_' + menu_item.name.replace(/\s/g, '');
-            var submenu = $(submenu_id);
-            generateMenu(menu_item.children, submenu);  //call the main generate_menu function  with new menu items and new parent_menu (depth first bit)
-        }
-
-    };
-
-    var generateMenuItem = function(menu_item, parent_menu) { //adds a new menu_item to the parent_menu
+    var generateMenuItem = function(menu_item, parent_menu) { //adds a a menu_item that DOES NOT contain a sub menu
           /*
           Example generated menu item (it has no submenu)
           <li>
@@ -84,5 +38,42 @@ $(document).ready(function() {
                         .append($('<input>', {class: "menu-checkbox", name: menu_item.name, type : "checkbox", onclick : "checkboxChanged(this)"}))
                         .append($('<span>', {text : ' ' + menu_item.name}))));
     };
+
+
+    var generateSubMenuItem = function(menu_item, parent_menu) { //adds another menu_item that DOES contain a sub menu
+
+        //construct children string
+        var children = [];
+        for(var i=0; i < menu_item.children.length; i++) {
+            children.push('{"name":"' + menu_item.children[i].name + '"}');
+        }
+        var children_string = '[ ' + children.join(',') + ']';
+        /*
+        Example Generate HTML
+        <li class="dropdown-submenu">
+            <li>
+                <a href="#" class="small" tabIndex="-1">
+                  <input type="checkbox" name="1" children='[{"name": "3.1"}, {"name": "3.2"}, {"name":"3.3"}]'>
+                  1
+                </a>
+            </li>
+            <ul class="dropdown-menu">
+                <!-- This is the submenu -->
+            </ul>
+        </li>
+        */
+        var newElem = $('<li>', {class : "dropdown-submenu"})
+                          .append($('<a>', { class: "small", tabIndex : "-1", href : "#" })
+                              .append($('<input>', { class: "menu-checkbox", name : menu_item.name, type : "checkbox", "data-children" : children_string, onclick : "checkboxChanged(this)"}))
+                              .append($('<span>', {text: ' ' + menu_item.name})))
+                          .append($('<ul>', {id: 'ul_' + menu_item.name.replace(/\s/g, ''), class : "dropdown-menu"}));
+
+        //Bit of trickery to get the submenus working
+        var tempElem = $(newElem).appendTo(parent_menu);
+        var submenu = $(tempElem).find('> ul');
+        generateMenu(menu_item.children, submenu);  //call the main generate_menu function with menu_items children and new parent_menu (depth first bit)
+    };
+
+
 
 });
