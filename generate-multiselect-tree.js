0023d7df6069c382bfacb94aa9aa4f4094c7346c
Tom Cartwright <tom.cartwright@barrachd.com>
2017-08-17 12:19:43 +0100

    made wireframe from menu item and submenu item

--- generate-multiselect-tree.js
+++ generate-multiselect-tree.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
-    console.log("hell!");
+    //Generating a standard menu item
+    var standard_menu_item = $('#mydropdownmenu')
+          .append($('<li>')
+              .append($('<a>')
+                  .append('<input>')));
+
+    //Generating menu item that contains a submenu
+    var submenu_menu_item = $('the_sub_menu_currently_in')
+          .append($('<li>')
+              .append($('<ul>')));
+
+
+    var generateSubMenu = function(current_menu) {
+
+    };
+
+    var generateMenuItem = function(current_menu) {
+
+    };
 
 });
